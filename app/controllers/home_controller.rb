90b9ab37784e1555ba46b74ac823d0fd4b17d6c6
Matt Anderson <matthewrossanderson@gmail.com>
2012-05-19 23:59:08 -0400

    Deleted coffeescripts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,12 @@
 class HomeController < ApplicationController
-
+  include Knodes
   
   def index
     @users = User.all
   end
   
   def proxy
-	puts "test"
+  	response = HTTParty.get('https://api.knod.es/customers.json?customer_id=4fb7e55a8e6396c715000009&customer_secret=09e7b248685712add0a71142177e3c31bcf824cc167e2266b2fca2c3a7df8db0')
+	render :text => response
   end
 end
