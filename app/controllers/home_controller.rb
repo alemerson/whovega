ea8e44c5b2d04040c9a7aa2f265592b2da3ad08b
Matt Anderson <matthewrossanderson@gmail.com>
2012-05-20 00:38:18 -0400

    Proxy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   end
   
   def proxy
-  	response = HTTParty.get('https://api.knod.es/customers.json?customer_id=4fb7e55a8e6396c715000009&customer_secret=09e7b248685712add0a71142177e3c31bcf824cc167e2266b2fca2c3a7df8db0')
-	render :text => response
+  	response = HTTParty.get(params[:url])
+	render :text => response.body
   end
 end
