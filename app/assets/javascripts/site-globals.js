bbdea9e05a9575b312cf0f5d6c8036bbdf7fcf34
Matt Anderson <matthewrossanderson@gmail.com>
2012-05-28 15:57:41 -0400

    Switch to localhost for dev

--- app/assets/javascripts/site-globals.js
+++ app/assets/javascripts/site-globals.js
@@ -1,11 +1,12 @@
 var DEBUG = false;
-//var DOMAIN = "localhost:3000";
-var DOMAIN = "mccreary.no-ip.biz:3000";
+var DOMAIN = "localhost:3000";
+//var DOMAIN = "mccreary.no-ip.biz:3000";
 var SITE_URL = "http://" + DOMAIN;
 
 var PROXY_URL = SITE_URL + "/proxy";
 //pat
 //var KNODES_USER = "4fb7ef5b8e63966e15000004";
+//vishal
 var KNODES_USER = "4fb7ffe58e6396c11500000f";
 var KNODES_URL = "https://api.knod.es/"
 var KNODES_ACCOUNT = "customer_id=4fb7e55a8e6396c715000009&customer_secret=09e7b248685712add0a71142177e3c31bcf824cc167e2266b2fca2c3a7df8db0&user_id=" + KNODES_USER;
